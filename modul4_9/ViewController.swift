66361b62b484ee5a5fd30dd1a918f35f5e7c2e6f
biloldin22 <biloldin22@mail.ru>
2023-06-23 14:34:56 +0900

    Initial Commit

---
+++ modul4_9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  modul4_9
+//
+//  Created by Mac on 2023/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
